21298a5f7c507cdce6e76af8176da45eb6150ed9
Robert Gilmore <Robert.Warren.Gilmore@gmail.com>
2014-10-03 18:31:18 -0400

    committing on behalf of @MStevens604: Added comments and cleaned up constructor calls.

--- src/Driver.java
+++ src/Driver.java
@@ -6,12 +6,28 @@ public class Driver {
      * @param args
      */
     public static void main(String[] args) {
-        boolean l = false;
-        int np = 4;
-        int nt = 10;
-        long tm = 0;
-        long em = 0;
+        boolean l = false; //left right disabled by default.  If enabled, even => right handed | odd => left handed
+        int np = 4; //numPhilos; numForks
+        int nt = 10; //num think/eat cycles
+        long tm = 0; //think time, millis
+        long em = 0; //eat time, millis
 
+        if (args.length == 5) {
+            if (args[0] == "-l") {
+                l = true;
+            }
+            np = Integer.valueOf(args[1]);
+            nt = Integer.valueOf(args[2]);
+            tm = Integer.valueOf(args[3]);
+            em = Integer.valueOf(args[4]);
+        } else if (args.length == 4) {
+            np = Integer.valueOf(args[0]);
+            nt = Integer.valueOf(args[1]);
+            tm = Integer.valueOf(args[2]);
+            em = Integer.valueOf(args[3]);
+        } else {
+            System.out.println("Invalid command line args.  Resorting to default values.");
+        }
         ArrayList<IFork> forks = new ArrayList<IFork>();
         ArrayList<Philosopher> philosophers = new ArrayList<Philosopher>();
         for (int i = 0; i < np; i++) {
