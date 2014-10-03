04a85ee6aa93c048f520f4744075915fe90760fa
Robert Gilmore <Robert.Warren.Gilmore@gmail.com>
2014-10-03 18:34:55 -0400

    More descriptive variable names.

--- src/Driver.java
+++ src/Driver.java
@@ -7,34 +7,34 @@ public class Driver {
      */
     public static void main(String[] args) {
         boolean l = false; //left right disabled by default.  If enabled, even => right handed | odd => left handed
-        int np = 4; //numPhilos; numForks
-        int nt = 10; //num think/eat cycles
-        long tm = 0; //think time, millis
-        long em = 0; //eat time, millis
+        int numPhilosophers = 4; // also the number of forks
+        int numCycles = 10; //num think/eat cycles
+        long thinkTime = 0; // milliseconds
+        long eatTime = 0; // milliseconds
 
         if (args.length == 5) {
             if (args[0] == "-l") {
                 l = true;
             }
-            np = Integer.valueOf(args[1]);
-            nt = Integer.valueOf(args[2]);
-            tm = Integer.valueOf(args[3]);
-            em = Integer.valueOf(args[4]);
+            numPhilosophers = Integer.valueOf(args[1]);
+            numCycles = Integer.valueOf(args[2]);
+            thinkTime = Integer.valueOf(args[3]);
+            eatTime = Integer.valueOf(args[4]);
         } else if (args.length == 4) {
-            np = Integer.valueOf(args[0]);
-            nt = Integer.valueOf(args[1]);
-            tm = Integer.valueOf(args[2]);
-            em = Integer.valueOf(args[3]);
+            numPhilosophers = Integer.valueOf(args[0]);
+            numCycles = Integer.valueOf(args[1]);
+            thinkTime = Integer.valueOf(args[2]);
+            eatTime = Integer.valueOf(args[3]);
         } else {
-            System.out.println("Invalid command line args.  Resorting to default values.");
+            System.out.println("Invalid command line args. Expected numPhilosophers, numCycles, thinkTime, eatTime.  Resorting to default values.");
         }
         ArrayList<IFork> forks = new ArrayList<IFork>();
         ArrayList<Philosopher> philosophers = new ArrayList<Philosopher>();
-        for (int i = 0; i < np; i++) {
+        for (int i = 0; i < numPhilosophers; i++) {
             forks.add(new Fork());
         }
-        for (int i = 0; i < np; i++) {
-            philosophers.add(new Philosopher(i, forks.get(i), forks.get((np + i - 1) % np), (l ? (i % 2 == 0 ? true : false) : true), nt, tm, em));
+        for (int i = 0; i < numPhilosophers; i++) {
+            philosophers.add(new Philosopher(i, forks.get(i), forks.get((numPhilosophers + i - 1) % numPhilosophers), (l ? (i % 2 == 0 ? true : false) : true), numCycles, thinkTime, eatTime));
             philosophers.get(i).start();
         }
     }
