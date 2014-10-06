4f0a28e74064e11e962395fb8e7a55c8e127cb14
cman131 <cgw5994@rit.edu>
2014-10-06 13:42:06 -0400

    added basic layout of files for the activity

---
+++ BankersAlgorithm/Banker.java
@@ -0,0 +1,28 @@
+public class Banker {
+	private int nUnits;
+	
+	public Banker(int nUnits){
+		this.nUnits = nUnits;
+	}
+	
+	public void setClaim(int nUnits){
+		Thread cur = Thread.currentThread();
+		
+	}
+	
+	public boolean request(int nUnits){
+		
+	}
+	
+	public void release(int nUnits){
+		
+	}
+	
+	public int allocated(){
+		
+	}
+	
+	public int remaining(){
+		
+	}
+}
---
+++ BankersAlgorithm/Client.java
@@ -0,0 +1,21 @@
+public class Client extends Thread{
+	
+	private Banker banker;
+	private int nUnits;
+	private int nRequests;
+	private long minSleepMillis;
+	private long maxSleepMillis;
+	
+	public Client(String name, Banker banker, int nUnits, int nRequests, long minSleepMillis, long maxSleepMillis){
+		super(name);
+		this.banker = banker;
+		this.nUnits = nUnits;
+		this.nRequests = nRequests;
+		this.minSleepMillis = minSleepMillis;
+		this.maxSleepMillis = maxSleepMillis;
+	}
+	
+	private void run(){
+		
+	}
+}
